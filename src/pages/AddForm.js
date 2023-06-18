34b2f3d01e7f5f6b5b4a4a80f4ce463a43a8bb21
rolandsobczak <rolandsobczak@icloud.com>
2023-06-18 02:12:07 +0200

    fix: post sending fixed

--- src/pages/AddForm.js
+++ src/pages/AddForm.js
@@ -1,76 +1,72 @@
 import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 
-import { API } from '../api/requests'
+import { API } from "../api/requests";
 
 export default function AddForm() {
-  const [content, setContent] = useState("");
-  const [image, setImage] = useState(null);
+    const [content, setContent] = useState("");
+    const [image, setImage] = useState(null);
 
-  async function onSubmit() {
-    const formData = new FormData();
-    formData.append("image", image);
-    formData.append("ngo", 0);
-    formData.append("content", content);
-    const { data } = await API.post("posts/", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
-  }
+    async function onSubmit() {
+        const formData = new FormData();
+        formData.append("image", image);
+        formData.append("ngo", 1);
+        formData.append("content", content);
+        const { data } = await API.post("posts/", formData, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+                "image-size": "small",
+            },
+        });
+        console.log(data);
+    }
 
-  function handleChangeField(e, callback) {
-    callback(e.target.value);
-  }
+    function handleChangeField(e, callback) {
+        callback(e.target.value);
+    }
 
-  function handleImageChange(event) {
-    setImage(event.target.files[0]);
-  }
+    function handleImageChange(event) {
+        setImage(event.target.files[0]);
+    }
 
-  return (
-    <div
-      className='position-absolute top-50 start-50 translate-middle d-flex h-75 w-75'
-      style={{ maxWidth: "1000px" }}
-    >
-      <Form
-        className='my-auto flex-grow-1'
-        onSubmit={(e) => {
-          e.preventDefault();
-          onSubmit();
-        }}
-      >
-        <h1 className='text-center'>Dodaj post</h1>
-        <Form.Group
-          className='mb-3'
-          controlId='formBasicEmail'
-          hasValidation
-          invalid={true}
+    return (
+        <div
+            className="position-absolute top-50 start-50 translate-middle d-flex h-75 w-75"
+            style={{ maxWidth: "1000px" }}
         >
-          <Form.Label>Treść</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Wprowadź treść posta'
-            value={content}
-            onChange={(e) => handleChangeField(e, setContent)}
-            invalid={true}
-          />
-          <Form.Label>Obraz</Form.Label>
-          <Form.Control
-            type='File'
-            onChange={(e) => handleImageChange(e, setImage)}
-            invalid={true}
-          />
-        </Form.Group>
-        <Button variant='primary' type='submit'>
-          Submit
-        </Button>
-      </Form>
-      <div className='border border-secondary mx-3 d-none d-md-block'></div>
-      <img
-        src='LibraryLogo.svg'
-        alt=''
-        className='w-25 my-auto d-none d-md-block'
-      />
-    </div>
-  );
+            <Form
+                className="my-auto flex-grow-1"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    onSubmit();
+                }}
+            >
+                <h1 className="text-center">Dodaj post</h1>
+                <Form.Group
+                    className="mb-3"
+                    controlId="formBasicEmail"
+                    hasValidation
+                    invalid={true}
+                >
+                    <Form.Label>Treść</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Wprowadź treść posta"
+                        value={content}
+                        onChange={(e) => handleChangeField(e, setContent)}
+                        invalid={true}
+                    />
+                    <Form.Label>Obraz</Form.Label>
+                    <Form.Control
+                        type="File"
+                        onChange={(e) => handleImageChange(e, setImage)}
+                        invalid={true}
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </div>
+    );
 }
